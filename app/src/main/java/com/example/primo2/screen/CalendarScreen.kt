51c65a5962f38afceb5395e11ab0ddb0e673a9ec
backfto <backfto@gmail.com>
2023-03-07 17:11:51 +0900

    캘린더 작업중2

--- app/src/main/java/com/example/primo2/screen/CalendarScreen.kt
+++ app/src/main/java/com/example/primo2/screen/CalendarScreen.kt
@@ -1,21 +1,19 @@
 package com.example.primo2.screen
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.rememberCalendarState
-import com.kizitonwose.calendar.core.CalendarDay
-import com.kizitonwose.calendar.core.daysOfWeek
-import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.core.*
 import java.time.*
 import java.time.format.TextStyle
 import java.util.*
@@ -27,7 +25,11 @@ fun Day(day: CalendarDay) {
             .aspectRatio(1f), // This is important for square sizing!
         contentAlignment = Alignment.Center
     ) {
-        Text(text = day.date.dayOfMonth.toString())
+        Text(
+            text = day.date.dayOfMonth.toString(),
+            color = if (day.position == DayPosition.MonthDate) Color.Black else Color.White
+        )
+
     }
 }
 
@@ -36,9 +38,11 @@ fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>) {
     Row(modifier = Modifier.fillMaxWidth()) {
         for (dayOfWeek in daysOfWeek) {
             Text(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier
+                    .weight(1f),
                 textAlign = TextAlign.Center,
                 text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
+                color = Color.Gray
             )
         }
     }
@@ -49,24 +53,27 @@ fun CalendarScreen(
     modifier: Modifier = Modifier
 ) {
     val currentMonth = remember { YearMonth.now() }
-    val startMonth = remember { currentMonth.minusMonths(100) } // Adjust as needed
-    val endMonth = remember { currentMonth.plusMonths(100) } // Adjust as needed
+    val startMonth = remember { currentMonth.minusMonths(2) } // Adjust as needed
+    val endMonth = remember { currentMonth.plusMonths(4) } // Adjust as needed
     val firstDayOfWeek = remember { firstDayOfWeekFromLocale() } // Available from the library
     val daysOfWeek = daysOfWeek()
     val state = rememberCalendarState(
         startMonth = startMonth,
         endMonth = endMonth,
         firstVisibleMonth = currentMonth,
-        firstDayOfWeek = firstDayOfWeek
+        firstDayOfWeek = firstDayOfWeek,
+        outDateStyle = OutDateStyle.EndOfRow
     )
 
     HorizontalCalendar(
+        modifier = Modifier,
         state = state,
         dayContent = { Day(it) },
         monthHeader = {
             DaysOfWeekTitle(daysOfWeek = daysOfWeek) // Use the title as month header
-        }
-
+        },
+        calendarScrollPaged = true,
+        contentPadding = PaddingValues(4.dp)
     )
 }
 
